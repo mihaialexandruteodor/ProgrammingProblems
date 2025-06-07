b7fbd3df7c9e17f0f7b13b3720a0e75c9754da4b
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-07 22:35:32 +0300

    feat: neetcode solution

--- PlayGround/problems/leetcode/647/PalindromicSubstrings.cs
+++ PlayGround/problems/leetcode/647/PalindromicSubstrings.cs
@@ -11,7 +11,43 @@ public class PalindromicSubstrings: IBaseSolution
         Console.WriteLine("abc, expect 3, actual: " + solution.CountSubstrings("abc"));
         Console.WriteLine("aaa, expect 6, actual: " + solution.CountSubstrings("aaa"));
     }
+
+    // neetcode solution, waaay more efficient -> O(n^2)
+    // https://www.youtube.com/watch?v=4RACzI5-du8
     public class Solution
+    {
+        public int CountSubstrings(string s)
+        {
+            int res = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                // Odd-length palindromes
+                int l = i, r = i;
+                while (l >= 0 && r < s.Length && s[l] == s[r])
+                {
+                    res++;
+                    l--;
+                    r++;
+                }
+
+                // Even-length palindromes
+                l = i;
+                r = i + 1;
+                while (l >= 0 && r < s.Length && s[l] == s[r])
+                {
+                    res++;
+                    l--;
+                    r++;
+                }
+            }
+
+            return res;
+        }
+    }
+
+    // my approach
+    /*public class Solution
     {
         public bool checkIfStringIsPalindrome(int stIndex, int edIndex, string s)
         {
@@ -39,5 +75,7 @@ public class PalindromicSubstrings: IBaseSolution
 
             return count;
         }
-    }
+    }*/
+
+
 }
