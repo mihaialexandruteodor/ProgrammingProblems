3f8de3e4ce08edf666456a65df5b5af921181aca
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-07 22:21:35 +0300

    feat: my solution

--- PlayGround/problems/leetcode/647/PalindromicSubstrings.cs
+++ PlayGround/problems/leetcode/647/PalindromicSubstrings.cs
@@ -7,15 +7,37 @@ public class PalindromicSubstrings: IBaseSolution
 
     public void solve()
     {
-        
-        /*to do - solver*/
+        Solution solution = new Solution();
+        Console.WriteLine("abc, expect 3, actual: " + solution.CountSubstrings("abc"));
+        Console.WriteLine("aaa, expect 6, actual: " + solution.CountSubstrings("aaa"));
     }
     public class Solution
     {
+        public bool checkIfStringIsPalindrome(int stIndex, int edIndex, string s)
+        {
+            if (string.IsNullOrEmpty(s) || stIndex < 0 || edIndex >= s.Length || stIndex > edIndex)
+                return false;
+
+            while (stIndex < edIndex)
+            {
+                if (s[stIndex] != s[edIndex])
+                    return false;
+                stIndex++;
+                edIndex--;
+            }
+
+            return true;
+        }
+
         public int CountSubstrings(string s)
         {
+            int count = s.Length;
+            for (int i = 0; i < s.Length - 1; ++i)
+                for (int j = i + 1; j < s.Length; ++j)
+                    if (checkIfStringIsPalindrome(i, j, s))
+                        count++;
 
-            return 0;
+            return count;
         }
     }
 }
