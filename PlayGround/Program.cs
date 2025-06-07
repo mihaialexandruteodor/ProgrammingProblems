61e2fd41622a67621f3f2f84a13d8995ce441bca
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-07 19:09:53 +0300

    feat: leetcode 647 boilerplate

--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -8,7 +8,7 @@ public class Program
 {
 	public static void Main()
 	{
-        IBaseSolution solution = new CloneGraphDeepCopy();
+        IBaseSolution solution = new PalindromicSubstrings();
 
         solution.solve();
     }
---
+++ PlayGround/problems/leetcode/647/PalindromicSubstrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PalindromicSubstrings: IBaseSolution
+{
+    //https://leetcode.com/problems/palindromic-substrings/
+
+    public void solve()
+    {
+        
+        /*to do - solver*/
+    }
+    public class Solution
+    {
+        public int CountSubstrings(string s)
+        {
+
+            return 0;
+        }
+    }
+}
