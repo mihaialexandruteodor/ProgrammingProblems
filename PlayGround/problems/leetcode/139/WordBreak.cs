bead522e7771e2ed4230b4f9fcfd4cff96176dd2
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-08 20:53:05 +0300

    feat: solution

--- PlayGround/problems/leetcode/139/WordBreak.cs
+++ PlayGround/problems/leetcode/139/WordBreak.cs
@@ -2,18 +2,40 @@
 using System.Collections.Generic;
 
 // https://leetcode.com/problems/word-break/
+// DYNAMIC PROGRAMMING
+// https://www.youtube.com/watch?v=Sx9NNgInc3A
 public class WordBreak : IBaseSolution
 {
     public void solve()
     {
-
+        Solution solution = new Solution();
+        Console.WriteLine("Expected True, got: " + solution.WordBreak("aaaaaaa", ["aaaa", "aaa"]));
+        Console.WriteLine("Expected True, got: " + solution.WordBreak("leetcode", ["leet", "code"]));
+        Console.WriteLine("Expected True, got: " + solution.WordBreak("applepenapple", ["apple", "pen"]));
+        Console.WriteLine("Expected False, got: " + solution.WordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]));
     }
-
     public class Solution
     {
         public bool WordBreak(string s, IList<string> wordDict)
         {
-            return false;
+            var wordSet = new HashSet<string>(wordDict);
+            var dp = new bool[s.Length + 1];
+            dp[0] = true; // empty string is always "breakable"
+
+            for (int i = 1; i <= s.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] && wordSet.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[s.Length];
         }
     }
+
 }
