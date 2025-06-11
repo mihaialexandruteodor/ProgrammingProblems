db850a6ed0ea583ee427577e280f238c2bbf3798
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-11 19:18:57 +0300

    feat: leetcode 20 ValidParentheses

--- PlayGround/problems/leetcode/20/ValidParentheses.cs
+++ PlayGround/problems/leetcode/20/ValidParentheses.cs
@@ -1,15 +1,45 @@
 ﻿public class ValidParentheses : IBaseSolution
 {
     // https://leetcode.com/problems/valid-parentheses/
+    // STACK PROBLEM
     public void solve()
     {
         Solution solution = new Solution();
-        Console.WriteLine("Expected : ");
-        Console.WriteLine("Actual: " );
+        Console.WriteLine("() , Expected : True");
+        Console.WriteLine("Actual: " + solution.IsValid("()"));
+        Console.WriteLine("()[]{} , Expected : True");
+        Console.WriteLine("Actual: " + solution.IsValid("()[]{}"));
+        Console.WriteLine("(] , Expected : False");
+        Console.WriteLine("Actual: " + solution.IsValid("(]"));
+        Console.WriteLine("([]) , Expected : True");
+        Console.WriteLine("Actual: " + solution.IsValid("([])"));
     }
 
     public class Solution
     {
-        
+        public bool IsValid(string s)
+        {
+
+            Dictionary<char, char> closedOpen = new();
+            closedOpen.Add(')', '(');
+            closedOpen.Add(']', '[');
+            closedOpen.Add('}', '{');
+            Stack<char> st = new();
+            foreach (char ch in s)
+            {
+                if (ch == ')' || ch == ']' || ch == '}')
+                {
+                    if (st.Count > 0 && closedOpen[ch] == st.Peek())
+                        st.Pop();
+                    else
+                        return false;
+                }
+                else
+                {
+                    st.Push(ch);
+                }
+            }
+            return st.Count == 0 ? true : false;
+        }
     }
 }
