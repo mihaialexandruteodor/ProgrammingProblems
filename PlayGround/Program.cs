926f8c0aad3c620dbc2d7dfb21f7881767df9a9f
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-11 18:50:38 +0300

    feat: leetcode 20 ValidParentheses boilerplate

--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -8,7 +8,7 @@ public class Program
 {
 	public static void Main()
 	{
-        IBaseSolution solution = new ReplaceWords();
+        IBaseSolution solution = new ValidParentheses();
 
         solution.solve();
     }
---
+++ PlayGround/problems/leetcode/20/ValidParentheses.cs
@@ -0,0 +1,15 @@
+﻿public class ValidParentheses : IBaseSolution
+{
+    // https://leetcode.com/problems/valid-parentheses/
+    public void solve()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine("Expected : ");
+        Console.WriteLine("Actual: " );
+    }
+
+    public class Solution
+    {
+        
+    }
+}
