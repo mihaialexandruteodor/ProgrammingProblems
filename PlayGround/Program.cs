c0c9071d7893fd195278c4da08bf03a1f5f52ffa
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-13 15:53:33 +0300

    feat: leetcode 152 max product subarray

--- PlayGround/Program.cs
+++ PlayGround/Program.cs
@@ -8,7 +8,7 @@ public class Program
 {
 	public static void Main()
 	{
-        IBaseSolution solution = new MergeKSortedLists();
+        IBaseSolution solution = new MaxProductSubarray();
 
         solution.solve();
     }
---
+++ PlayGround/problems/leetcode/152/MaxProductSubarray.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+[Description("MaxProductSubarray")]
+public class MaxProductSubarray : IBaseSolution
+{
+    // 
+    // STACK PROBLEM
+    public void solve()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine("[2,3,-2,4], Expected : 6");
+        Console.WriteLine("Actual: " + solution.MaxProduct([2, 3, -2, 4]));
+    }
+
+    public class Solution
+    {
+        public int MaxProduct(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            int maxProd = nums[0];
+            int minProd = nums[0];
+            int result = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int tempMax = maxProd;
+                maxProd = Math.Max(nums[i], Math.Max(maxProd * nums[i], minProd * nums[i]));
+                minProd = Math.Min(nums[i], Math.Min(tempMax * nums[i], minProd * nums[i]));
+                result = Math.Max(result, maxProd);
+            }
+
+            return result;
+        }
+    }
+}
