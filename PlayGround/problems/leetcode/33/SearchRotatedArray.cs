fd7b19c4b4334c64e2152e3eebaab081fd45db84
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-15 17:25:03 +0300

    feat: leetcode 33 boilerplate

---
+++ PlayGround/problems/leetcode/33/SearchRotatedArray.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+[Description("SearchRotatedArray")]
+public class SearchRotatedArray : IBaseSolution
+{
+    // https://leetcode.com/problems/search-in-rotated-sorted-array/
+    public void solve()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine("[4,5,6,7,0,1,2], t=0, Expected : 4");
+        Console.WriteLine("Actual: " + solution.Search([4, 5, 6, 7, 0, 1, 2], 0));
+    }
+
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            return 0;
+        }
+    }
+}
