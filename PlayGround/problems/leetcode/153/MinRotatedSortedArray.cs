08e326c8b4e08960d15352d57cca2514a1b5a48c
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-06-15 17:13:48 +0300

    feat: MinRotatedSortedArray

--- PlayGround/problems/leetcode/153/MinRotatedSortedArray.cs
+++ PlayGround/problems/leetcode/153/MinRotatedSortedArray.cs
@@ -8,12 +8,28 @@ public class MinRotatedSortedArray : IBaseSolution
     public void solve()
     {
         Solution solution = new Solution();
-        Console.WriteLine("Expected : ");
-        Console.WriteLine("Actual: ");
+        Console.WriteLine("[4,5,6,7,0,1,2], Expected : 0");
+        Console.WriteLine("Actual: " + solution.FindMin([4, 5, 6, 7, 0, 1, 2]));
     }
 
     public class Solution
     {
+        public int FindMin(int[] nums)
+        {
+            if (nums[0] < nums[nums.Length - 1] || nums.Length == 1)
+                return nums[0];
 
+            int min = int.MaxValue;
+            int i = 0;
+            while (i < nums.Length - 1)
+            {
+                if (nums[i] > nums[i + 1])
+                    return nums[i + 1];
+                if (nums[i] < min)
+                    min = nums[i];
+                ++i;
+            }
+            return min;
+        }
     }
 }
