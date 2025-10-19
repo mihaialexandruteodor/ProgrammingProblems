0ad9b88a77b88c0265c73e3ed4fa16a8228e88f5
mihaialexandruteodor <alexandruteodormihai@proton.me>
2025-10-19 11:12:14 +0300

    feat: leetcode 1625 boilerplate

---
+++ Java/app/src/main/java/org/example/leetcode/problem1625/Solution1625.java
@@ -0,0 +1,9 @@
+package org.example.leetcode.problem1625;
+
+public class Solution1625 {
+
+    public static void runProblem() {
+        System.out.println("Expected: 2050");
+        System.out.println("Actual: ");
+    }
+}
